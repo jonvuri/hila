efc093bdf14bb340e91da1b4362fdea5c50ad9c1
Jon Rajavuori <jrajav@gmail.com>
2025-07-04 09:16:07 -0500

    Simplify sql/client a bit

--- src/sql/client.ts
+++ src/sql/client.ts
@@ -1,56 +1,40 @@
-import type {
-  ExecuteResultMessage,
-  ExecuteErrorMessage,
-  WorkerMessage,
-  SqlResult,
-} from './types'
+import type { WorkerMessage, SqlResult } from './types'
 
 const worker = new Worker(new URL('./worker.ts', import.meta.url), { type: 'module' })
 
-type ExecuteResultListener = (data: ExecuteResultMessage | ExecuteErrorMessage) => void
-
-const executeResultListeners = new Map<string, Set<ExecuteResultListener>>()
-
-const addExecuteResultListener = (sql: string, listener: ExecuteResultListener) => {
-  const listeners = executeResultListeners.get(sql)
-
-  if (listeners) {
-    listeners.add(listener)
-  } else {
-    executeResultListeners.set(sql, new Set([listener]))
-  }
-}
+// Keep track of all pending queries for a given SQL statement (the string key),
+// to deduplicate them. All queries are resolved or rejected together.
+const pendingQueries = new Map<
+  string,
+  { resolve: (result: SqlResult) => void; reject: (error: Error) => void }[]
+>()
 
 worker.onmessage = (event: MessageEvent<WorkerMessage>) => {
   const { type } = event.data
 
   if (type === 'executeResult') {
     const { sql } = event.data
+    const resolvers = pendingQueries.get(sql)
 
-    const listeners = executeResultListeners.get(sql)
-
-    if (listeners) {
-      for (const listener of listeners) {
-        listener(event.data)
+    if (resolvers) {
+      for (const { resolve } of resolvers) {
+        resolve(event.data.result)
       }
-
-      executeResultListeners.delete(sql)
+      pendingQueries.delete(sql)
     } else {
-      console.warn(`No listener for execute result of ${sql}`)
+      throw new Error(`No pending queries for execute result of ${sql}`)
     }
   } else if (type === 'executeError') {
     const { sql } = event.data
+    const resolvers = pendingQueries.get(sql)
 
-    const listeners = executeResultListeners.get(sql)
-
-    if (listeners) {
-      for (const listener of listeners) {
-        listener(event.data)
+    if (resolvers) {
+      for (const { reject } of resolvers) {
+        reject(event.data.error)
       }
-
-      executeResultListeners.delete(sql)
+      pendingQueries.delete(sql)
     } else {
-      console.warn(`No listener for execute error of ${sql}`)
+      throw new Error(`No pending queries for execute error of ${sql}`)
     }
   } else if (type === 'log') {
     console.log(event.data.message)
@@ -60,15 +44,13 @@ worker.onmessage = (event: MessageEvent<WorkerMessage>) => {
 }
 
 export const executeSql = (sql: string, bind?: (string | number | boolean)[]) => {
-  worker.postMessage({ type: 'execute', sql, bind })
-
   return new Promise<SqlResult>((resolve, reject) => {
-    addExecuteResultListener(sql, (data) => {
-      if (data.type === 'executeResult') {
-        resolve(data.result)
-      } else if (data.type === 'executeError') {
-        reject(data.error)
-      }
-    })
+    const existing = pendingQueries.get(sql)
+    if (existing) {
+      existing.push({ resolve, reject })
+    } else {
+      pendingQueries.set(sql, [{ resolve, reject }])
+      worker.postMessage({ type: 'execute', sql, bind })
+    }
   })
 }
